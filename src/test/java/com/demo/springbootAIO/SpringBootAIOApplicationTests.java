7c98dc70e618af696f4f7f18244f6d079409daf8
ben <ben@intella.co>
2022-06-13 11:34:31 +0800

    docker init

---
+++ src/test/java/com/demo/springbootAIO/SpringBootAIOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootAIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAIOApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

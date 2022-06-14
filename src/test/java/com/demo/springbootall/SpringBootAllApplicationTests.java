3aad04cba3f27045b032d461eb27d860f5da2407
benpengtw <0083gp03@gmail.com>
2022-06-14 15:59:04 +0800

    add mybatis

--- src/test/java/com/demo/springbootall/SpringBootAllApplicationTests.java
+++ src/test/java/com/demo/springbootall/SpringBootAllApplicationTests.java
@@ -1,13 +1,16 @@
 package com.demo.springbootall;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringBootAllApplicationTests {
+public class SpringBootAllApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
